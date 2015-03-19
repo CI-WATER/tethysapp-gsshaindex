01931e626e90691107cab5abc5db90320f1b0308
anderjoc <jocelynn.anderson@gmail.com>
2015-03-19 14:55:35 -0600

    Replace index values with shapefile ids

--- tethysapp/gsshaindex/controllers/select_shapefile.py
+++ tethysapp/gsshaindex/controllers/select_shapefile.py
@@ -62,8 +62,6 @@ def shapefile_index(request, job_id, index_name, shapefile_name):
     dataset_engine = get_spatial_dataset_engine(name='gsshaindex_geoserver', app_class=GSSHAIndex)
     overlay_result = gi_lib.get_layer_and_resource(dataset_engine, user, shapefile_name)
 
-    print overlay_result['resource']['wfs']['geojson']
-
     if overlay_result['success'] == True:
         url = overlay_result['layer']['wms']['kml']
         coord_list = list(overlay_result['resource']['latlon_bbox'][:-1])
@@ -213,14 +211,13 @@ def get_geojson_from_geoserver(user, shapefile_name):
         url = overlay_result['resource']['wfs']['geojson']
         r= requests.get(url)
         status = r.status_code
-        print "STATUS: ", status
         if status == 200:
             result =  r.json()
-            geojson =  str(result['features'])
-            print "RESULT: ", result
-            return JsonResponse({'geojson':geojson, 'success':True})
+            geojson =  result['features']
+            srid = result['crs']
+            return {'geojson':geojson, 'crs':srid, 'success':True}
         else:
-            return JsonResponse({'success':False})
+            return {'success':False}
 
 
 def replace_index_with_shapefile(request, job_id, index_name, shapefile_name):
@@ -232,13 +229,154 @@ def replace_index_with_shapefile(request, job_id, index_name, shapefile_name):
 
     geojson = get_geojson_from_geoserver(user, shapefile_name)
 
-    print geojson
+    # Get the job from the database
+    job_session = jobs_sessionmaker()
+    job, success = gi_lib.get_pending_job(job_id, user, job_session)
+    CKAN_engine = get_dataset_engine(name='gsshaindex_ciwweb', app_class=GSSHAIndex)
+
+    # Get project file id
+    project_file_id = job.new_model_id
+
+    # Create a session
+    gsshapy_session = gsshapy_sessionmaker()
+
+    # Specify the workspace
+    controllerDir = os.path.abspath(os.path.dirname(__file__))
+    gsshaindexDir = os.path.abspath(os.path.dirname(controllerDir))
+    publicDir = os.path.join(gsshaindexDir,'public')
+    userDir = os.path.join(publicDir, str(user))
+    indexMapDir = os.path.join(userDir, 'index_maps')
+
+    # Clear the workspace
+    gi_lib.clear_folder(userDir)
+    gi_lib.clear_folder(indexMapDir)
+
+    # Use project id to link to original map table file
+    project_file = gsshapy_session.query(ProjectFile).filter(ProjectFile.id == project_file_id).one()
+    index_raster = gsshapy_session.query(IndexMap).filter(IndexMap.mapTableFile == project_file.mapTableFile).filter(IndexMap.name == index_name).one()
+
+    mapTables = index_raster.mapTables
 
     if geojson['success'] == False:
         print "PANIC"
     else:
         geojson_result = geojson['geojson']
 
+        # Delete existing indices
+        index_raster_indices = index_raster.indices
+        for index in index_raster_indices:
+            bob = gsshapy_session.query(MTIndex).get(index.id)
+            for val in bob.values:
+                gsshapy_session.delete(val)
+            gsshapy_session.delete(bob)
+
+        srid_name = geojson['crs']
+
+        project_file_srid = project_file.srid
+
+        id = 1
+
+        # Loop through each geometry
+        for object in geojson_result:
+            index_present = False
+            object_id = object['id']
+            for index in index_raster_indices:
+                if object_id == index.index:
+                    index_present = True
+                    break
+
+            # Create new index value if it doesn't exist and add the number of ids
+            if index_present == False:
+                new_indice = MTIndex(id, object_id,"")
+                new_indice.indexMap = index_raster
+                for mapping_table in mapTables:
+                    distinct_vars = gsshapy_session.query(distinct(MTValue.variable)).\
+                                     filter(MTValue.mapTable == mapping_table).\
+                                     order_by(MTValue.variable).\
+                                     all()
+                    variables = []
+                    for var in distinct_vars:
+                        variables.append(var[0])
+
+                    for variable in variables:
+                        print variable
+                        new_value = MTValue(variable, 0)
+                        new_value.mapTable = mapping_table
+                        new_value.index = new_indice
+                    gsshapy_session.commit()
+
+            geom = object['geometry']
+            geom['crs'] = srid_name
+            geom_full = json.dumps(geom)
+
+            # Change values in the index map
+            statement = "SELECT ST_SetValue(raster,1,ST_Transform(ST_GeomFromGeoJSON('{0}'), {1}),{2}) FROM idx_index_maps WHERE id = {3};".format(geom_full, project_file_srid, id, index_raster.id)
+
+            result = gsshapy_engine.execute(statement)
+            id += 1
+
+        # Get the values in the index map
+        statement3 = '''SELECT (pvc).*
+                        FROM (SELECT ST_ValueCount(raster,1,true) As pvc
+                        FROM idx_index_maps WHERE id = '''+ unicode(index_raster.id) +''') AS foo
+                        ORDER BY (pvc).value;
+                        '''
+        result3 = gsshapy_engine.execute(statement3)
+
+        numberIDs = 0
+        ids = []
+        for row in result3:
+            numberIDs +=1
+            ids.append(row.value)
+            print ids
+
+        map_table_count = 0
+        for mapping_table in mapTables:
+
+            index_raster.mapTables[map_table_count].numIDs = numberIDs
+
+            indices = gsshapy_session.query(distinct(MTIndex.index), MTIndex.id, MTIndex.description1, MTIndex.description2).\
+                                   join(MTValue).\
+                                   filter(MTValue.mapTable == mapping_table).\
+                                   order_by(MTIndex.index).\
+                                   all()
+
+            for index in indices:
+                if not int(index[0]) in ids:
+                    bob = gsshapy_session.query(MTIndex).get(index.id)
+                    for val in bob.values:
+                        gsshapy_session.delete(val)
+                    gsshapy_session.delete(bob)
+            gsshapy_session.commit()
+            map_table_count +=1
+
+        index_raster =  gsshapy_session.query(IndexMap).filter(IndexMap.mapTableFile == project_file.mapTableFile).filter(IndexMap.name == index_name).one()
+
+        # Create kml file name and path
+        current_time = time.strftime("%Y%m%dT%H%M%S")
+        resource_name = index_raster.name + "_" + str(user) + "_" + current_time
+        kml_ext = resource_name + '.kml'
+        clusterFile = os.path.join(indexMapDir, kml_ext)
+
+        # Generate color ramp
+        index_raster.getAsKmlClusters(session=gsshapy_session, path=clusterFile, colorRamp=ColorRampEnum.COLOR_RAMP_HUE, alpha=0.6)
+
+        index_map_dataset = gi_lib.check_dataset("index-maps", CKAN_engine)
+        resource, status = gi_lib.add_kml_CKAN(index_map_dataset, CKAN_engine, clusterFile, resource_name)
+
+        temp_list = json.loads(job.current_kmls)
+
+        if status == True:
+            for item in temp_list:
+                    if item == index_name:
+                        del temp_list[item]
+                        temp_list[index_name] = {'url':resource['url'], 'full_name':resource['name']}
+                        break
+
+        job.current_kmls = json.dumps(temp_list)
+        job_session.commit()
+        job_session.close()
+        gsshapy_session.close()
 
     context['index_name'] = index_name
     context['job_id'] = job_id
